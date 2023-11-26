bffbffff13da0923f85347d3206b8ee3fbe9cadd
zTom1909 <topolococo2@gmail.com>
2023-11-26 15:53:14 -0300

    add(Home): Added text and button to the home image

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -15,13 +15,22 @@ const Home = () => {
   
   return (
     <div className="flex flex-col items-center text-navText font-humanist777 bg-whiteBackground">
-      <div className="w-full h-[540px] mb-8 overflow-hidden grid grid-cols-12 gap-8">
+      <div className="w-full h-fit mb-8 overflow-hidden grid grid-cols-12 gap-8">
         <div></div>
-        <img
-          src={image1}
-          alt="about"
-          className="w-full mt-[-120px] col-span-10"
-        />
+        <div className="w-full h-[540px] relative col-span-10">
+          <img
+            src={image1}
+            alt="about"
+            className="w-full mt-[-120px]"
+          />
+          <div className="w-72 left-20 bottom-32 absolute text-whiteBackground font-humanist777 drop-shadow-2xl">
+            <h1 className="mb-2 text-2xl">Quienes Somos?</h1>
+            <p className="mb-8 text-sm">Somos una empresa familiar con una trayectoria que abarca tres generaciones.</p>
+            <button onClick={() => navigate("/about")} className="rounded-lg px-[16px] py-[8px] text-white text-xl text-bold font-feltTipRoman bg-[#D93D33] hover:bg-[#DF5D55] drop-shadow-md">
+              Mas Información
+            </button>
+          </div>
+        </div>
       </div>
       <h1 className="mb-1 text-3xl text-bold">Nuestros Productos</h1>
       <p className="mb-8 text-sm">Mira un listado de nuestros productos</p>
