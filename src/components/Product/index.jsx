79065b4824f6368b6bb84e4d21bad6d2469c4f74
zTom1909 <topolococo2@gmail.com>
2024-01-03 01:50:38 -0300

    fix(Product): Fixed product text hitting its container borderds

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -1,10 +1,8 @@
 const Product = ({ name, image }) => {
   return (
     <div className="max-w-[240px] rounded-lg flex flex-col justify-end overflow-hidden bg-productBackground drop-shadow-md">
-      <div className="bg-cover bg-center lg:h-[180px] md:h-[120px] h-[80px]" style={{backgroundImage: `url(${image})`}}>
-        {/* <img src={image} alt={name} /> */}
-      </div>
-      <p className="lg:h-[48px] md:h-[36px] h-[24px] lg:p-2 md-p2 p-1 flex justify-center items-center lg:text-base md:text-sm text-xs text-white bg-productName">
+      <div className="bg-cover bg-center lg:h-[180px] md:h-[120px] h-[80px]" style={{backgroundImage: `url(${image})`}} />
+      <p className="lg:h-[48px] md:h-[36px] h-[24px] lg:p-2 md-p2 p-2 flex justify-center items-center lg:text-base md:text-sm text-[9px] text-white bg-productName">
         {name}
       </p>
     </div>
