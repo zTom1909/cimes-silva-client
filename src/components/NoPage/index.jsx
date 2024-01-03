d4d8e6af06484669fcca2793cf2c34fcd30d582c
zTom1909 <topolococo2@gmail.com>
2024-01-03 01:56:47 -0300

    feat(404): Added responsiveness to 404 page

--- src/components/NoPage/index.jsx
+++ src/components/NoPage/index.jsx
@@ -9,7 +9,7 @@ const NoPage = () => {
       <div className="z-0 absolute w-screen h-screen top-0 bg-cover bg-center bg-no-page blur-sm"></div>
       <img src={logo} alt="water planet" className="z-10 w-[240px]" />
       <h1 className="z-10 mt-[-48px] text-navText text-[8rem] font-bold">404</h1>
-      <p className="z-10 mb-4 text-navText text-xl">
+      <p className="z-10 lg:max-w-[800px] md:max-w-[600px] max-w-[280px] mb-8 text-navText text-xl">
         Parece que esta página no existe! Presiona el boton para volver al inicio
         </p>
       <button onClick={() => navigate("/home")}
